11f1e202f42d0299ce3a4948cf5dfbe16c8e73e8
a.barkook <albarkook@gmail.com>
2022-05-27 17:28:58 +0430

    update -> update q-3

--- HW-3/q-3/q-3.cpp
+++ HW-3/q-3/q-3.cpp
@@ -1,13 +1,10 @@
-// CPP program to implement Queue using
-// two stacks with costly enqueue()
 #include <bits/stdc++.h>
 using namespace std;
 
 struct Queue {
 	stack<int> s1, s2;
 
-	void enqueue(int x)
-	{
+	void enqueue(int a) {
 		// Move all elements from s1 to s2
 		while (!s1.empty()) {
 			s2.push(s1.top());
@@ -15,7 +12,7 @@ struct Queue {
 		}
 
 		// Push item into s1
-		s1.push(x);
+		s1.push(a);
 
 		// Push everything back to s1
 		while (!s2.empty()) {
@@ -24,9 +21,7 @@ struct Queue {
 		}
 	}
 
-	// dequeue an item from the queue
-	int dequeue()
-	{
+	int dequeue() {
 		// if first stack is empty
 		if (s1.empty()) {
 			cout << "Q is Empty";
@@ -40,20 +35,28 @@ struct Queue {
 	}
 
     void size() {
+		cout<<s1.size()<<endl;
+    }
 
+    void isEmpty() {
+
+		if (s1.size() > 0)
+			cout<<"bale"<<endl;
+		else
+			cout<<"bale"<<endl;
     }
 };
 
-// Driver code
-int main()
-{
+
+
+int main() {
 	Queue q;
 
     int ch;
    cout<<"1) Insert element to queue"<<endl;
    cout<<"2) Delete element from queue"<<endl;
    cout<<"3) Display size of queue"<<endl;
-//    cout<<"4) Display all the elements of queue"<<endl;
+   cout<<"5) check queue is empty"<<endl;
    cout<<"4) Exit"<<endl;
    do {
       cout<<"Enter your choice : "<<endl;
@@ -70,8 +73,8 @@ int main()
          break;
          case 3: q.size();
          break;
-        //  case 4: q.Display();
-        //  break;
+         case 4: q.isEmpty();
+         break;
          case 5: cout<<"Exit"<<endl;
          break;
          default: cout<<"Invalid choice"<<endl;
@@ -79,13 +82,4 @@ int main()
    } while(ch!=5);
    return 0;
 
-	// q.enqueue(1);
-	// q.enqueue(2);
-	// q.enqueue(3);
-
-	// cout << q.dequeue() << '\n';
-	// cout << q.dequeue() << '\n';
-	// cout << q.dequeue() << '\n';
-
-	// return 0;
 }
